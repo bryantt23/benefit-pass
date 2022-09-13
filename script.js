a2c868f5c161e56cb713b159268817ab43d6017e
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-09-13 13:07:31 -0700

    initial

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-alert('hii');
+const steps = {
+  steps: {
+    'reverse-text': {
+      type: 'reverse-string',
+      args: {
+        string: {
+          type: 'reference',
+          step: 'get-text'
+        }
+      }
+    },
+    'get-text': {
+      type: 'http-request',
+      args: {
+        url: {
+          type: 'value',
+          value: 'https://www.example.com'
+        }
+      }
+    },
+    'save-file': {
+      type: 'save-file',
+      args: {
+        filename: {
+          type: 'value',
+          value: '/foo.txt'
+        },
+        contents: {
+          type: 'reference',
+          step: 'reverse-text'
+        }
+      }
+    }
+  }
+};
+
+console.log(steps);
