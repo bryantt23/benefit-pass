14ede7a43d04ee71a572c61f98226504707f3acb
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-09-13 13:48:40 -0700

    trying to add to queue

--- script.js
+++ script.js
@@ -43,8 +43,9 @@ console.log(JSON.stringify(obj, null, 4));
 //get ones that have no steps
 //take out the step, remove from args of other steps, repeat
 const steps = obj.steps;
-const stepsToExecute = [];
+const stepsToExecute = []; //like queue of steps
 const completedSteps = new Set();
+
 for (const key in steps) {
   let hasStepProperty = false;
   const nestedObject = steps[key];
@@ -61,8 +62,48 @@ for (const key in steps) {
   }
   if (!hasStepProperty) {
     stepsToExecute.push(nestedObject);
+    completedSteps.add(key);
   }
 }
+
+while (stepsToExecute.length > 0) {
+  const curTask = stepsToExecute.pop();
+  const { type } = curTask;
+  if (type === 'http-request') {
+    const res = http_request(curTask['args']['url']['value']);
+  }
+
+  //iterate through object, remove current step
+
+  //   for (const key in steps) {
+  //     debugger;
+  //     let hasStepProperty = false;
+  //     const nestedObject = steps[key];
+  //     const argsObject = nestedObject['args'];
+  //     //   console.log('🚀 ~ file: script.js ~ line 51 ~ argsObject', argsObject);
+  //     for (const nestedKey in argsObject) {
+  //       // console.log('🚀 ~ file: script.js ~ line 53 ~ nestedKey', nestedKey);
+  //       const val = argsObject[nestedKey];
+  //       console.log('🚀 ~ file: script.js ~ line 55 ~ val', val);
+  //       // console.log(Object.keys(val));
+  //       if (Object.keys(val).includes('step')) {
+  //         hasStepProperty = true;
+  //       }
+  //     }
+  //     if (!hasStepProperty) {
+  //       stepsToExecute.push(nestedObject);
+  //       completedSteps.add(key);
+  //     }
+  //   }
+
+  //iterate through object, add step without dependencies
+}
+
+const functionsObject = {
+  'http-request': http_request,
+  'reverse-string': string_reverse,
+  'save-file': save_file
+};
 
 console.log('🚀 ~ file: script.js ~ line 47 ~ ', stepsToExecute); //gettext
 //helper functions
