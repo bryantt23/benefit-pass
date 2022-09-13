33802a3bfd6ec9a5b1085fd91d7987191d77b96e
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-09-13 13:33:02 -0700

    add step with no steps required

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const steps = {
+const obj = {
   steps: {
     'reverse-text': {
       type: 'reverse-string',
@@ -34,8 +34,37 @@ const steps = {
   }
 };
 
-console.log(JSON.stringify(steps, null, 4));
+console.log(JSON.stringify(obj, null, 4));
 
+//goal topological sort
+//add steps to an array
+
+//use queue
+//get ones that have no steps
+//take out the step, remove from args of other steps, repeat
+const steps = obj.steps;
+const stepsToExecute = [];
+const completedSteps = new Set();
+for (const key in steps) {
+  let hasStepProperty = false;
+  const nestedObject = steps[key];
+  const argsObject = nestedObject['args'];
+  //   console.log('🚀 ~ file: script.js ~ line 51 ~ argsObject', argsObject);
+  for (const nestedKey in argsObject) {
+    // console.log('🚀 ~ file: script.js ~ line 53 ~ nestedKey', nestedKey);
+    const val = argsObject[nestedKey];
+    // console.log('🚀 ~ file: script.js ~ line 55 ~ val', val);
+    // console.log(Object.keys(val));
+    if (Object.keys(val).includes('step')) {
+      hasStepProperty = true;
+    }
+  }
+  if (!hasStepProperty) {
+    stepsToExecute.push(nestedObject);
+  }
+}
+
+console.log('🚀 ~ file: script.js ~ line 47 ~ ', stepsToExecute); //gettext
 //helper functions
 function http_request(url) {
   if (url === 'https://www.example.com') {
